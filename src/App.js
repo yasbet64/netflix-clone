9aaf4accb8e175a175ed1c94dcb83440d8fe3507
Dixroby Arone Cuellar <dixroby.arone@itsight.pe>
2021-10-21 08:05:13 -0500

    Add Proyecto Clonado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner'
+import Nav from './Nav';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav/>
+      <Banner/>
+    
+    
+      <Row 
+      title="NETFLIX ORIGINAL" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow={true}
+      />
+      
+        <Row title="Peliculas Nuevas" fetchUrl={requests.fetchTrending} />
+       <Row title="Top de Peliculas" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Peliculas de Acción" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Peliculas de Comedia" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Peliculas de Terror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Peliculas de Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentales" fetchUrl={requests.fetchDocumentaries} /> 
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,42 @@
+import React,{ useState,useEffect} from 'react'
+import "./Nav.css"
+
+function Nav () {
+
+    const [show, handleShow] = useState(false);
+
+   useEffect(() => {
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY > 100){
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return()=>{
+            window.removeEventListener("scroll");
+        };
+    }, []);
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+           <img
+           className="nav__logo"
+           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" 
+           alt="Netflix Logo"
+           /> 
+
+           <div className="nav__usuario">
+           <h3 className="nav__nombre">Yasbeth_AR</h3>
+           <h4 className="nav__cargo">(Developer)</h4>
+           </div>
+
+           
+
+            <img
+            className="nav__avatar"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="Netflix Logo"
+            /> 
+        </div>
+    )
+}
+
+export default Nav
